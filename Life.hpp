e685338ff41f81a23a8e40329153e52d38648184
camkuwa <camillekuwahara@gmail.com>
2025-04-23 14:55:27 -0500

    change life

--- Life.hpp
+++ Life.hpp
@@ -8,48 +8,7 @@
 #include <vector>
 #include <cstddef>
 
-class AbstractCell {
-protected:
-    bool _state;
-
-public:
-    AbstractCell(bool state);
-    virtual ~AbstractCell();
-    virtual AbstractCell* clone() const = 0;
-    virtual void evolve() = 0;
-};
-
-class ConwayCell : public AbstractCell {
-public:
-    ConwayCell(bool state);
-    ConwayCell* clone() const override;
-    void evolve() override;
-};
-
-class FredkinCell : public AbstractCell {
-private:
-    size_t _age;
-
-public:
-    FredkinCell(bool state, size_t age);
-    FredkinCell* clone() const override;
-    void evolve() override;
-};
-
-class Cell {
-private:
-    AbstractCell* _p;
-
-public:
-    Cell(AbstractCell*);
-    Cell(const Cell&);
-    Cell(Cell&&);
-    Cell& operator=(const Cell&);
-    Cell& operator=(Cell&&);
-    ~Cell();
-    void evolve();
-};
-
+//Class life
 template <typename T>
 class Life {
 private:
@@ -58,6 +17,58 @@ private:
 public:
     Life(size_t rows, size_t cols);
     void eval();
+};
+
+class Cell {
+    private:
+        AbstractCell* _p;
+    
+    public:
+        Cell(AbstractCell*);
+        Cell(const Cell&);
+        Cell(Cell&&);
+        Cell& operator=(const Cell&);
+        Cell& operator=(Cell&&);
+        ~Cell();
+        void evolve();
+    };
+
+
+class AbstractCell {
+protected:
+    //the state of the cell
+    bool _state;
+    
+
+public:
+    AbstractCell(bool state);
+    //virtual destructor
+    virtual ~AbstractCell() = default;
+    virtual AbstractCell* clone() const = 0;
+    virtual void evolve() = 0;
+};
+
+//Conway Cell
+class ConwayCell : public AbstractCell {
+public:
+    //default constructor
+    ConwayCell () = default;
+    ConwayCell(bool state);
+    ConwayCell* clone() const override;
+    //deconstructor
+    ~ ConwayCell() = default;
+    void evolve() override;
+};
+
+//Fredkin Cell cell
+class FredkinCell : public AbstractCell {
+private:
+    size_t _age;
+
+public:
+    FredkinCell(bool state, size_t age);
+    FredkinCell* clone() const override;
+    void evolve() override;
 };
 
 
