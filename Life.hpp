39d0271ea65a4e6ae8611ba5d26f4716203166c5
camkuwa <camillekuwahara@gmail.com>
2025-04-24 17:58:44 -0500

    update life again

--- Life.hpp
+++ Life.hpp
@@ -10,15 +10,18 @@ using namespace std;
 // AbstractCell base class
 class AbstractCell {
 protected:
-    bool _state;
+    bool _state; // boolean whether the cell is alive
 
 public:
     AbstractCell(bool state) : _state(state) {}
     virtual ~AbstractCell() = default;
-
+    //Clone method for copying
     virtual AbstractCell* clone() const = 0;
+    //Cell evolves between generations
     virtual void evolve() = 0;
+    // Returns the current state of the cell
     virtual bool state() const = 0;
+    //check if cell is alive
     bool isAlive() const {
         return _state;
     }
@@ -31,7 +34,7 @@ public:
     ConwayCell(bool state) : AbstractCell(state) {}
     //destructor
     ~ConwayCell() = default;
-
+    //updates the state
     void evolve() override {
         _state = !_state;
     }
@@ -49,13 +52,13 @@ public:
 // FredkinCell
 class FredkinCell : public AbstractCell {
 private:
-    size_t _age;
+    size_t _age; //Tracks the number of evolutiosn while alive
 
 public:
     FredkinCell(bool state = false) : AbstractCell(state), _age(0) {}
     
     ~FredkinCell() = default;
-
+    //increments age if alive
     void evolve() override {
         if (_state) {
             ++_age;
@@ -69,14 +72,17 @@ public:
 
 };
 
-// Cell wrapper
+// Cell wrapper to manage dynamic AbstractCe instances
 class Cell {
 private:
     AbstractCell* _pointer;
 
 public:
+    //deep copy
     Cell(AbstractCell* cell) : _pointer(cell->clone()) {}
+    //copy constructor for uses clone
     Cell(const Cell& other) : _pointer(other._pointer->clone()) {}
+    //move constructor
     Cell(Cell&& other) noexcept : _pointer(other._pointer) { other._pointer = nullptr; }
 
     Cell& operator=(Cell&& other) noexcept {
@@ -102,29 +108,32 @@ public:
 template <typename T>
 class Life {
 private:
-    vector<vector<T>> _grid;
+    vector<vector<T>> _grid; // 2D grid of cells
 
 public:
+    // Initializes grid
     Life(size_t rows, size_t cols) {
         _grid.resize(rows, vector<T>(cols));
     }
-
+    // Advances the simulation to one generation
     void eval() {
-        for (auto& row : _grid) {
-            for (auto& cell : row) {
+        for_each(_grid.begin(), _grid.end(), [](auto& row) {
+            for_each(row.begin(), row.end(), [](auto& cell) {
                 cell.evolve();
-            }
-        }
+            });
+        });
     }
-
+    
+    // Prints the board
     void print() const {
-        for (const auto& row : _grid) {
-            for (const auto& cell : row) {
-                cell.print();
+        for (size_t i = 0; i < _grid.size(); ++i) {
+            for (size_t j = 0; j < _grid[i].size(); ++j) {
+                _grid[i][j].print();
             }
             cout << '\n';
         }
     }
+ 
 };
 
 #endif // Life_hpp
